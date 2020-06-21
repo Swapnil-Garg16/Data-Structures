6e5bee748aaaf2c728293cbb99864c0a085bf900
Swapnil Garg <10002001@hmel.int>
2020-06-21 21:07:30 +0530

    nth node from the end of LL

---
+++ Linked List/nth node from the end of Linked List.cpp
@@ -0,0 +1,98 @@
+//nth node from the end of Linked List
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+	int data;
+	struct Node * next;
+};
+
+//inserting in a linked list
+void insert(struct Node **head,int data)
+{
+	struct Node * newNode =(struct Node *) (malloc)(sizeof(struct Node));
+	struct Node *temp = *head;
+	newNode->data = data;
+
+	if(*head==NULL)
+	{
+		*head=newNode;
+	newNode->next=NULL;
+	}
+	else
+	{
+			
+			while(temp->next!=NULL)
+			{
+				temp=temp->next;
+			}
+			temp->next = newNode;
+			newNode->next=NULL;
+	}
+}
+
+void print(struct Node *head)
+{
+	struct Node * temp = head;
+	while(temp!=NULL)
+	{
+		printf("%d ",temp->data);
+		temp=temp->next;
+	}
+	
+	printf("\n");
+}
+
+int countNodes(struct Node *head)
+{
+	struct Node *temp = head;
+	int count=0;
+	while(temp!=NULL)
+	{
+		count++;
+		temp=temp->next;
+	}
+	
+	return count;
+}
+
+void nthNodeFromEnd(struct Node *head, int n)
+{
+	int count = countNodes(head);
+	if(n>count)
+	{
+		printf("Node doesn't exist\n");
+		return ;
+	}
+	struct Node *temp = head;
+	count=count-n+1;
+	int pos = 1;
+	while(pos!=count && temp!=NULL)
+	{
+		temp=temp->next;
+		pos++;
+	}
+	
+	printf("%d\n",temp->data);
+	
+}
+
+int main()
+{
+	int n,i,pos;
+	struct Node * head = NULL;
+	for(i=0;i<10;i++){
+	printf("Enter the data to be added \n");
+	scanf("%d",&n);
+	insert(&head,n);
+	print(head);
+}
+for(i=0;i<4;i++)
+{
+printf("Enter n\n");
+scanf("%d",&n);
+nthNodeFromEnd(head,n);
+
+}
+	
+}
